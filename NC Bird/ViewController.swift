064c3f9f9fc4b9d06b2991d4f6d513ba6c1bbb16
Jasmine Hanifa Mounir <jasmine.mounir@gmail.com>
2019-05-18 23:32:07 +0700

    Insert background and bird

--- NC Bird/ViewController.swift
+++ NC Bird/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var birdA: UIImageView!
+    @IBOutlet weak var birdB: UIImageView!
+    @IBOutlet weak var birdC: UIImageView!
+   
+    var birdAImage: [UIImage] = []
+    var birdBImage: [UIImage] = []
+    var birdCImage: [UIImage] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        birdAImage = createImageArray(total: 4, imagePrefix: "birdA")
+        birdBImage = createImageArray(total: 4, imagePrefix: "birdB")
+        birdCImage = createImageArray(total: 4, imagePrefix: "birdC")
     }
 
+    func createImageArray(total: Int, imagePrefix: String) -> [UIImage]{
+        
+        var imageArray:[UIImage] = []
+        
+        for imageCount in 0..<total{
+            let imageName = "\(imagePrefix)-\(imageCount).png"
+            let image = UIImage(named: imageName)!
+            
+            imageArray.append(image)
+        }
+        
+        return imageArray
+    }
 
 }
 
