4bddb13d3af333aa9d898514c868514b1b2092a2
Jasmine Hanifa Mounir <jasmine.mounir@gmail.com>
2019-05-18 17:20:21 +0700

    Initial Commit

---
+++ NC Bird/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NC Bird
+//
+//  Created by Jasmine Hanifa Mounir on 18/05/19.
+//  Copyright © 2019 Jasmine Hanifa Mounir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
